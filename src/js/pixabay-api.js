e56eec94b035cdf7d2c29830f36aca3d8aab70d2
Natalia_Tolstykh <kopylova9624@gmail.com>
2026-01-17 14:33:30 +0100

    my hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_KEY = "54240480-a4799bfc66af4dde196d1db7d";
+const BASE_URL = "https://pixabay.com/api/";
+
+export async function getImagesByQuery(query) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+    },
+  });
+
+  return response.data;
+}
